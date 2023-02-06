3cc37b964363d8931f89d5ee5fcd53e8413b324c
marips <99839074+marianafeitosa@users.noreply.github.com>
2023-02-06 16:33:51 -0300

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$a = 1;
+$b = 2;
+
+echo "Adição:" . $a + $b . "<br>";
+echo "Subtração:" . $a - $b . "<br>"; 
+echo "Multiplicação:" . $a * $b . "<br>";
+echo "Dvisão:" . $a / $b . "<br>"; 
+
+?>
